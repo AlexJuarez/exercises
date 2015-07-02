f4bc6222f180b80e9c0a882d023ae3c221e2dca3
Alexander Juarez <couatli@gmail.com>
2015-07-01 19:04:44 -0700

    throttle seems to be broken :(

--- sort/index.js
+++ sort/index.js
@@ -1,2 +1,29 @@
-module.exports = function () {
+module.exports = function quickSort(arr) {
+  var pivot = arr[0];
+
+  var lessThan = [];
+  var greaterThan = [];
+  var equalTo = [];
+
+  for (var i = 1; i < arr.length; i++) {
+    if(arr[i] > pivot) {
+      greaterThan.push(arr[i]);
+    } else if (arr[i] < pivot) {
+      lessThan.push(arr[i]);
+    } else {
+      equalTo.push(arr[i]);
+    }
+  }
+
+  equalTo.push(pivot);
+
+  if(lessThan.length > 1) {
+    lessThan = quickSort(lessThan);
+  }
+  
+  if(greaterThan.length > 1) {
+    greaterThan = quickSort(greaterThan);
+  }
+
+  return Array.prototype.concat(lessThan, equalTo, greaterThan);
 }
---
+++ throttle/index.js
@@ -0,0 +1,25 @@
+module.exports = function (fn, time) {
+  var args;
+  var lastCalled;
+  var ctx;
+  var timeout;
+  
+  return function () {
+    ctx = this;
+    args = [].slice.call(arguments);
+    
+    if (!lastCalled || ((new Date) - lastCalled > time && !timeout)) {
+      lastCalled = new Date;
+      fn.apply(ctx, args);
+    }
+
+    if (!timeout) {
+      timeout = setTimeout(function() {
+        lastCalled = new Date;
+        fn.apply(ctx, args);
+        timeout = null;
+      }, time);
+    }
+    
+  }
+}
